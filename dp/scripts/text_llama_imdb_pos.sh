8a2b6750b0395ce1cc3fe78f8b5bb39d76a84420
min-sy <nsy728@ewhain.net>
2024-01-03 10:56:59 +0900

    ...

--- dp/scripts/text_llama_imdb_pos.sh
+++ dp/scripts/text_llama_imdb_pos.sh
@@ -6,24 +6,22 @@ python main.py \
 --noise_multiplier 1.0 \
 --lookahead_degree 1 \
 --data_folder /home/minsy/CUBIG/dp/data/IMDB/private/positive \
---num_samples_schedule 100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100 \
---variation_degree_schedule 1.0,0.98,0.96,0.94,0.92,0.88,0.84,0.8,0.76,0.72,0.68,0.64,0.6,0.56,0.52,0.48,0.44,0.4 \
---num_private_samples 100 \
---initial_prompt "Generate BATCH positive movie reviews as if they were posted on IMDB. The review does not have a title, a number, or anything other than itself." \
---make_fid_stats True \
---compute_fid True \
---num_fid_samples 100 \
+--num_samples_schedule 10,10,10 \
+--variation_degree_schedule 1.0,0.98,0.96 \
+--num_private_samples 10 \
+--initial_prompt "Generate a positive movie reviews as if they were posted on IMDB. The review does not have a title, a number, or anything other than itself." \
+--make_fid_stats false \
+--compute_fid false \
+--num_fid_samples 10 \
 --fid_model_name clip_vit_b_32 \
 --fid_dataset_name imdb_pos_llama \
 --result_folder /home/minsy/CUBIG/dp/result/IMDB/llama/positive \
 --tmp_folder /tmp/IMDB/pos/llama \
 --api llama2 \
---random_sampling_checkpoint meta-llama/Llama-2-7b-chat-hf \
+--random_sampling_checkpoint togethercomputer/Llama-2-7B-32K-Instruct \
 --random_sampling_batch_size 4 \
---variation_checkpoint meta-llama/Llama-2-7b-chat-hf \
+--variation_checkpoint togethercomputer/Llama-2-7B-32K-Instruct \
 --variation_batch_size 1 \
---max_seq_len 512 \
---top_k 10 \
 --api_device 2 \
 --epsilon 1.0 \
 --delta 0.0 \
